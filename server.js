85610428a8f886a9a28d0c8b8622ff259be21a89
Zain Ali <zainop001@gmail.com>
2026-02-21 13:03:44 +0500

    fixes

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
